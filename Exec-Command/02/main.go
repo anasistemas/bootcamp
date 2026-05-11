e9ac86ef211196b15dec996e326288754ee0e9f2
anasofiasistemas <agoyeneche@sistemasenlinea.com.co>
2026-05-11 14:28:18 -0500

    Ejercicio 2: Ejecutar un comando y mostrar la salida

---
+++ Exec-Command/02/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("ls")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("El comando falló:", err)
+		return
+	}
+}
