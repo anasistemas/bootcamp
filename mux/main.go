9c99510661c5586fef130af5fa41f4cb1e12a766
anasofiasistemas <agoyeneche@sistemasenlinea.com.co>
2026-05-20 15:27:46 -0500

    Assigment: Can You Handle This?

--- mux/main.go
+++ mux/main.go
@@ -3,21 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"mux/handlers"
 )
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is the home page."))
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is the about page."))
-}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", homeHandler)
-	mux.HandleFunc("/about", aboutHandler)
+	home := handlers.HomeHandler{}
+	mux.Handle("/", home)
+
+	mux.Handle("/about", http.HandlerFunc(handlers.AboutHandler))
+
+	mux.HandleFunc("/help", handlers.HelpHandler)
+
+	log.Println("Server running on :9090")
 
 	log.Fatal(http.ListenAndServe(":9090", mux))
 }
