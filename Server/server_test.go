3e3a3755d00058ac0d94a45825d2dd0e50a7493a
anasofiasistemas <agoyeneche@sistemasenlinea.com.co>
2026-05-26 16:18:42 -0500

    Assigment: Todo - Table-Driven tests

--- Server/server_test.go
+++ Server/server_test.go
@@ -8,6 +8,13 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	name            string
+	path            string
+	expectedCode    int
+	expectedContent string
+}
+
 func setupAPI(t *testing.T) (url string, cleaner func()) {
 	t.Helper()
 	server := httptest.NewServer(newMux())
@@ -18,60 +25,53 @@ func setupAPI(t *testing.T) (url string, cleaner func()) {
 	return url, cleaner
 }
 
-func TestGetRoot(t *testing.T) {
-	path := "/"
-	expectedCode := http.StatusOK
-	expectedContent := "Hello World"
+func TestGet(t *testing.T) {
+	cases := []testCase{
+		{
+			name:            "Root",
+			path:            "/",
+			expectedCode:    http.StatusOK,
+			expectedContent: "Hello World",
+		},
+		{
+			name:            "NotFound",
+			path:            "/blog",
+			expectedCode:    http.StatusNotFound,
+			expectedContent: "404",
+		},
+	}
 
 	url, cleaner := setupAPI(t)
 	defer cleaner()
 
-	resp, err := http.Get(url + path)
-	if err != nil {
-		t.Fatalf("error haciendo la petición: %v", err)
-	}
-	defer resp.Body.Close()
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := http.Get(url + tc.path)
+			if err != nil {
+				t.Fatalf("error haciendo la petición: %v", err)
+			}
+			defer resp.Body.Close()
 
-	if resp.StatusCode != expectedCode {
-		t.Errorf("esperaba status %s, obtuve %s",
-			http.StatusText(expectedCode),
-			http.StatusText(resp.StatusCode))
-	}
+			if resp.StatusCode != tc.expectedCode {
+				t.Errorf("esperaba status %s, obtuve %s",
+					http.StatusText(tc.expectedCode),
+					http.StatusText(resp.StatusCode))
+			}
 
-	buf := new(strings.Builder)
-	io.Copy(buf, resp.Body)
-	body := buf.String()
+			buf := new(strings.Builder)
+			io.Copy(buf, resp.Body)
+			body := buf.String()
 
-	if !strings.Contains(body, expectedContent) {
-		t.Errorf("esperaba contenido %q, obtuve %q", expectedContent, body)
-	}
-}
-
-func TestGetNotFound(t *testing.T) {
-	path := "/blog"
-	expectedCode := http.StatusNotFound
-	expectedContent := "404"
-
-	url, cleaner := setupAPI(t)
-	defer cleaner()
-
-	resp, err := http.Get(url + path)
-	if err != nil {
-		t.Fatalf("error haciendo la petición: %v", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != expectedCode {
-		t.Errorf("esperaba status %s, obtuve %s",
-			http.StatusText(expectedCode),
-			http.StatusText(resp.StatusCode))
-	}
-
-	buf := new(strings.Builder)
-	io.Copy(buf, resp.Body)
-	body := buf.String()
-
-	if !strings.Contains(body, expectedContent) {
-		t.Errorf("esperaba contenido %q, obtuve %q", expectedContent, body)
+			switch resp.Header.Get("Content-Type") {
+			case "text/plain; charset=utf-8":
+				if !strings.Contains(body, tc.expectedContent) {
+					t.Errorf("esperaba contenido %q, obtuve %q",
+						tc.expectedContent, body)
+				}
+			default:
+				t.Fatalf("Unsupported Content-Type: %q",
+					resp.Header.Get("Content-Type"))
+			}
+		})
 	}
 }
