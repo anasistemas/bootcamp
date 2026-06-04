a1548df8c1b2a1e99cfef9ab180a1c4aebf31425
anasofiasistemas <agoyeneche@sistemasenlinea.com.co>
2026-06-04 11:04:15 -0500

    Assigment: Todo - GET Todo By ID

--- Server/handlers.go
+++ Server/handlers.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/anasistemas/todo"
 )
@@ -23,6 +27,24 @@ func router(datafile string) http.HandlerFunc {
 			return
 		}
 
+		parts := strings.Split(r.URL.Path, "/")
+
+		if len(parts) == 3 && parts[2] != "" {
+			id, err := validateID(parts[2], &list)
+			if err != nil {
+				errorReply(w, r, http.StatusBadRequest, err.Error())
+				return
+			}
+
+			switch r.Method {
+			case http.MethodGet:
+				getOneHandler(w, r, &list, id)
+			default:
+				errorReply(w, r, http.StatusMethodNotAllowed, "method not allowed")
+			}
+			return
+		}
+
 		switch r.Method {
 		case http.MethodGet:
 			getAllHandler(w, r, &list)
@@ -34,8 +56,28 @@ func router(datafile string) http.HandlerFunc {
 	}
 }
 
+func validateID(idStr string, list *todo.List) (int, error) {
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return 0, fmt.Errorf("id inválido: %q no es un número", idStr)
+	}
+	if id < 1 {
+		return 0, errors.New("id debe ser mayor o igual a 1")
+	}
+	if id > len(*list) {
+		return 0, fmt.Errorf("id %d no existe en la lista", id)
+	}
+	return id, nil
+}
+
 func getAllHandler(w http.ResponseWriter, r *http.Request, list *todo.List) {
 	resp := &todoResponse{Results: *list}
+	jsonReply(w, r, http.StatusOK, resp)
+}
+
+func getOneHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int) {
+	result := todo.List{(*list)[id-1]}
+	resp := &todoResponse{Results: result}
 	jsonReply(w, r, http.StatusOK, resp)
 }
 
